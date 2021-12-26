d5e8127d35b175d8bddbda0366380056b0a5370a
Zarrar Khan <zarrar.khan@pnnl.gov>
2021-12-26 16:41:50 -0500

    Updating package

--- R/chart.R
+++ R/chart.R
@@ -5,11 +5,13 @@
 #' @param chart_type Default = "all". Choices one or more of "all", "lines", "bar", "lines_diff", "bar_diff"
 #' @param col_agg Default = "class". Column to remove and then aggregate by.
 #' @param aspect_ratio Default = 0.75. aspect ratio
-#' @param size Default = 10. text size
-#' @param size_title Default = NULL. size_title text
+#' @param size Default = 1.5. line size
+#' @param size_text Default = NULL. Text size
 #' @param theme Default = NULL.
 #' @param ncol Default = 3. Number of columns.
 #' @param scales Default = "free". Choose between "free", "free_y", "free_x", "fixed"
+#' @param save Default = TRUE. Save plots.
+#' @param folder Default = getwd().
 #' @importFrom magrittr %>%
 #' @importFrom data.table :=
 #' @export
@@ -18,13 +20,28 @@ chart <- function(data = NULL,
                   col_agg = "class",
                   chart_type = "all",
                   aspect_ratio = 0.75,
-                  size = 10,
-                  size_title = NULL,
+                  size = 1.5,
+                  size_text = 10,
                   theme = NULL,
-                  ncol = 3,
-                  scales = "free"){
+                  ncol = NULL,
+                  scales = "free_y",
+                  save = TRUE,
+                  folder = getwd()){
 
   print("Starting chart...")
+
+  # Check
+  # data = NULL
+  # col_agg = "class"
+  # chart_type = "all"
+  # aspect_ratio = 0.75
+  # size = 10
+  # size_title = NULL
+  # theme = NULL
+  # ncol = 3
+  # scales = "free"
+  # save = TRUE
+  # folder = getwd()
 
   #...........................
   # Initialize
@@ -33,24 +50,44 @@ chart <- function(data = NULL,
   charts_out <- list()
   count <- 1
 
+  if(save){
+    if(!dir.exists(folder)){dir.create(folder)}
+  }
+
   #.................................
   # Prepare Data
   #.................................
 
   data_full <- rchart::add_missing(data)
-
   data_agg = rchart::aggregate_data(data = data_full, col_agg = col_agg)
+  n_param = length(unique(data_agg$param))
+  if(is.null(ncol)){ncol = as.integer(ceiling(n_param^0.5))}
+  width_i = 7*max(ncol^0.5,1)
+  height_i = 5*max((n_param-ncol)^0.5,1)
 
   #.................................
   # Summary Plot comparing scenarios absolute
   #.................................
 
   chart_name_i <- "chart_summary"
-  charts_out[count] <- rchart::plot_summary(data = data_agg,
+  fname_i <- paste0(folder,"/",chart_name_i,".png")
+  charts_out[[count]] <- rchart::plot_summary(data = data_agg,
                                             aspect_ratio = aspect_ratio,
                                             size = size,
-                                            size_title = size_title,
-                                            theme = theme)
+                                            size_text = size_text,
+                                            theme = theme,
+                                            ncol = ncol,
+                                            scales = scales)
+  names(charts_out)[count] <- chart_name_i
+
+  if(save){ggplot2::ggsave(filename = fname_i,
+                           plot = charts_out[[count]],
+                           width = width_i,
+                           height = height_i,
+                           units = "in")
+
+    print(paste0("Figure saved as: ",fname_i))
+    }
 
   #.................................
   # Summary Plot comparing scenarios diff absolute
@@ -71,11 +108,6 @@ chart <- function(data = NULL,
   #.................................
   # Bar Chart comparing scenarios Diff Percent
   #.................................
-
-
-  #...........................
-  # Theme
-  #...........................
 
 
   #...........................
--- R/plot_summary.R
+++ R/plot_summary.R
@@ -3,8 +3,8 @@
 #' generate summary plot
 #' @param data Default = NULL.
 #' @param aspect_ratio Default = 0.75. aspect ratio
-#' @param size Default = 10. text size
-#' @param size_title Default = NULL. size_title text
+#' @param size Default = 1.5. Line size
+#' @param size_text Default = 10. text size
 #' @param theme Default = NULL.
 #' @param ncol Default = 3. Number of columns.
 #' @param scales Default = "free". Choose between "free", "free_y", "free_x", "fixed"
@@ -13,22 +13,17 @@
 
 plot_summary <- function(data = NULL,
                          aspect_ratio = 0.75,
-                         size = 10,
-                         size_title = NULL,
+                         size = 1.5,
+                         size_text = 10,
                          theme = NULL,
                          ncol = 3,
-                         scales = "free") {
+                         scales = "free_y") {
 
   #...........................
   # Initialize
   #...........................
 
   NULL->x->value->scenario
-
-  if(is.null(size_title)){
-    size_title = size*1.2
-  }
-
 
   #...........................
   # Plot
@@ -39,7 +34,7 @@ plot_summary <- function(data = NULL,
                         group=scenario,
                         color=scenario)) +
     ggplot2::theme_bw() +
-    ggplot2::geom_line(size=2) +
+    ggplot2::geom_line(size=size) +
     ggplot2::ylab(NULL) +
     ggplot2::xlab(NULL) +
     ggplot2::facet_wrap(
@@ -49,10 +44,11 @@ plot_summary <- function(data = NULL,
           labeller = ggplot2::labeller(param = ggplot2::label_wrap_gen(15))
         ) +
     ggplot2::theme(legend.position="top",
-              legend.size_title = ggplot2::element_blank(),
+              legend.title = ggplot2::element_blank(),
               plot.margin = ggplot2::margin(20,20,20,0,"pt"),
-              text = ggplot2::element_text(size=size),
-              aspect.ratio = aspect_ratio)
+              text = ggplot2::element_text(size=size_text),
+              aspect.ratio = aspect_ratio,
+              axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
 
   if(!is.null(theme)){p1 <- p1 + theme}
 
