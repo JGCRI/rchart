9d7c3290bf4c7ae97cf2ffbc95c26932d60f1d62
Zarrar Khan <zarrar.khan@pnnl.gov>
2021-12-28 10:20:16 -0500

    updating docs

--- R/add_missing.R
+++ R/add_missing.R
@@ -15,24 +15,31 @@ add_missing <- function(data){
                                scenario=dplyr::case_when(is.na(scenario)~"scenario",TRUE~scenario))}
   if(!any(grepl("\\<scenarios\\>",names(data),ignore.case = T))){}else{
     data <- data %>% dplyr::rename(!!"scenario" := (names(data)[grepl("\\<scenarios\\>",names(data),ignore.case = T)])[1])
-    data<-data%>%dplyr::mutate(scenario=dplyr::case_when(is.na(scenario)~"scenario",TRUE~scenario))}
+    data<-data%>%dplyr::mutate(scenario = as.character(scenario),
+                               scenario=dplyr::case_when(is.na(scenario)~"scenario",TRUE~scenario))}
   if(!any(grepl("\\<region\\>",names(data),ignore.case = T))){data<-data%>%dplyr::mutate(region="region")}else{
     data <- data %>% dplyr::rename(!!"region" := (names(data)[grepl("\\<region\\>",names(data),ignore.case = T)])[1])
-    data<-data%>%dplyr::mutate(region=dplyr::case_when(is.na(region)~"region",TRUE~region))}
+    data<-data%>%dplyr::mutate(region = as.character(region),
+                               region=dplyr::case_when(is.na(region)~"region",TRUE~region))}
   if(!any(grepl("\\<subRegion\\>",names(data),ignore.case = T))){
     if(!any(grepl("\\<region\\>",names(data),ignore.case = T))){data<-data%>%dplyr::mutate(subRegion="region")
-    }else{data<-data%>%dplyr::mutate(subRegion="subRegion")}}else{
+    }else{data<-data%>%dplyr::mutate(subRegion = as.character(subRegion),
+                                     subRegion="subRegion")}}else{
     data <- data %>% dplyr::rename(!!"subRegion" := (names(data)[grepl("\\<subRegion\\>",names(data),ignore.case = T)])[1])
-    data<-data%>%dplyr::mutate(subRegion=dplyr::case_when(is.na(subRegion)~"subRegion",TRUE~subRegion))}
+    data<-data%>%dplyr::mutate(subRegion = as.character(subRegion),
+                               subRegion=dplyr::case_when(is.na(subRegion)~"subRegion",TRUE~subRegion))}
   if(!any(grepl("\\<subRegions\\>",names(data),ignore.case = T))){}else{
     data <- data %>% dplyr::rename(!!"subRegion" := (names(data)[grepl("\\<subRegions\\>",names(data),ignore.case = T)])[1])
-    data<-data%>%dplyr::mutate(subRegion=dplyr::case_when(is.na(subRegion)~"subRegion",TRUE~subRegion))}
+    data<-data%>%dplyr::mutate(subRegion = as.character(subRegion),
+                               subRegion=dplyr::case_when(is.na(subRegion)~"subRegion",TRUE~subRegion))}
   if(!any(grepl("\\<param\\>",names(data),ignore.case = T))){data<-data%>%dplyr::mutate(param="param")}else{
     data <- data %>% dplyr::rename(!!"param" := (names(data)[grepl("\\<param\\>",names(data),ignore.case = T)])[1])
-    data<-data%>%dplyr::mutate(param=dplyr::case_when(is.na(param)~"param",TRUE~param))}
+    data<-data%>%dplyr::mutate(param = as.character(param),
+                               param=dplyr::case_when(is.na(param)~"param",TRUE~param))}
   if(!any(grepl("\\<params\\>",names(data),ignore.case = T))){}else{
     data <- data %>% dplyr::rename(!!"param" := (names(data)[grepl("\\<params\\>",names(data),ignore.case = T)])[1])
-    data<-data%>%dplyr::mutate(param=dplyr::case_when(is.na(param)~"param",TRUE~param))}
+    data<-data%>%dplyr::mutate(param = as.character(param),
+                               param=dplyr::case_when(is.na(param)~"param",TRUE~param))}
   if(!any(grepl("\\<value\\>",names(data),ignore.case = T))){stop("Data must have 'value' column.")}else{
     data <- data %>% dplyr::rename(!!"value" := (names(data)[grepl("\\<value\\>",names(data),ignore.case = T)])[1])
     data$value = as.numeric(data$value)
@@ -43,25 +50,30 @@ add_missing <- function(data){
     data<-data%>%dplyr::mutate(value=dplyr::case_when(is.na(value)~0,TRUE~value))}
   if(!any(grepl("\\<unit\\>",names(data),ignore.case = T))){}else{
     data <- data %>% dplyr::rename(!!"units" := (names(data)[grepl("\\<unit\\>",names(data),ignore.case = T)])[1])
-    data<-data%>%dplyr::mutate(units=dplyr::case_when(is.na(units)~"units",TRUE~units))}
+    data<-data%>%dplyr::mutate(units = as.character(units),
+                               units=dplyr::case_when(is.na(units)~"units",TRUE~units))}
   if(!any(grepl("\\<units\\>",names(data),ignore.case = T))){data<-data%>%dplyr::mutate(units="units")}else{
     data <- data %>% dplyr::rename(!!"units" := (names(data)[grepl("\\<units\\>",names(data),ignore.case = T)])[1])
-    data<-data%>%dplyr::mutate(units=dplyr::case_when(is.na(units)~"units",TRUE~units))}
+    data<-data%>%dplyr::mutate(units = as.character(units),
+                               units=dplyr::case_when(is.na(units)~"units",TRUE~units))}
   if(!"x"%in%names(data)){
     if("year"%in%names(data)){
       data<-data%>%dplyr::mutate(x=year)}else{data<-data%>%dplyr::mutate(x="x")}}
   if(!any(grepl("\\<aggregate\\>",names(data),ignore.case = T))){
     if(is.null(aggregate)){data<-data%>%dplyr::mutate(aggregate="sum")}else{
-      data<-data%>%dplyr::mutate(aggregate="sum")}
+      data<-data%>%dplyr::mutate(aggregate = as.character(aggregate),
+                                 aggregate="sum")}
   }else{
     data <- data %>% dplyr::rename(!!"aggregate" := (names(data)[grepl("\\<aggregate\\>",names(data),ignore.case = T)])[1])
-    data<-data%>%dplyr::mutate(aggregate=dplyr::case_when(is.na(aggregate)~"sum",
+    data<-data%>%dplyr::mutate(aggregate = as.character(aggregate),
+                               aggregate=dplyr::case_when(is.na(aggregate)~"sum",
                                                           TRUE~aggregate))}
   if(!any(grepl("\\<class\\>",names(data),ignore.case = T))){
     if(!any(grepl("\\<class\\>",names(data),ignore.case = T))){
       data<-data%>%dplyr::mutate(class="class")}else{data<-data%>%dplyr::mutate(class=class)}}else{
         data <- data %>% dplyr::rename(!!"class" := (names(data)[grepl("\\<class\\>",names(data),ignore.case = T)])[1])
-        data<-data%>%dplyr::mutate(class=dplyr::case_when(is.na(class)~"class",TRUE~class))}
+        data<-data%>%dplyr::mutate(class = as.character(class),
+                                   class=dplyr::case_when(is.na(class)~"class",TRUE~class))}
 
   data <- data %>%
     dplyr::select(scenario,region,subRegion,param,class,x,aggregate,value) %>%
