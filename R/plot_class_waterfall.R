445f8541b20b135ccee2969d431c03e910d6575e
Taryn Waite <taryn.waite17@gmail.com>
2022-10-25 12:58:21 -0400

    adding regional waterfall capability

--- R/plot_class_waterfall.R
+++ R/plot_class_waterfall.R
@@ -26,26 +26,27 @@
 #' @export
 
 plot_class_waterfall <- function(data_diff = NULL,
-                                  data_agg = NULL,
-                                  scenRef = NULL,
-                                  scenDiff = NULL,
-                                  theme = NULL,
-                                  theme_default = ggplot2::theme_bw(),
-                                  diff_text = NULL,
-                                  scales = "free",
-                                  diff_type = "bar",
-                                  size = 1.5,
-                                  break_interval = NULL,
-                                  include_points = FALSE,
-                                  summary_line = FALSE,
-                                  wf_x = NULL,
-                                  rect_width = 0.7,
-                                  aspect_ratio = 0.6,
-                                  horizontal_lines = TRUE,
-                                  lty = 2,
-                                  fill_colors = NULL,
-                                  totals_fill_color = "gray90",
-                                  palette = NULL){
+                                 data_agg = NULL,
+                                 scenRef = NULL,
+                                 scenDiff = NULL,
+                                 theme = NULL,
+                                 theme_default = ggplot2::theme_bw(),
+                                 diff_text = NULL,
+                                 scales = "free",
+                                 diff_type = "bar",
+                                 size = 1.5,
+                                 break_interval = NULL,
+                                 include_points = FALSE,
+                                 summary_line = FALSE,
+                                 wf_x = NULL,
+                                 rect_width = 0.7,
+                                 aspect_ratio = 0.6,
+                                 horizontal_lines = TRUE,
+                                 lty = 2,
+                                 fill_colors = NULL,
+                                 totals_fill_color = "gray90",
+                                 palette = NULL,
+                                 vertical_dim = NULL){
 
   #...........................
   # Initialize
@@ -57,6 +58,20 @@ plot_class_waterfall <- function(data_diff = NULL,
   data_diff_subset <- data_diff %>%
     dplyr::filter(x == wf_x)
 
+  # add a dummy column if there's no vertical dimension
+  if(is.null(vertical_dim)){
+    data_diff_subset <- data_diff_subset %>%
+      dplyr::mutate(vDim = "vDim")
+    data_agg <- data_agg %>%
+      dplyr::mutate(vDim = "vDim")
+  } else{
+    data_diff_subset <- data_diff_subset %>%
+      dplyr::rename(vDim = tidyselect::all_of(vertical_dim))
+    data_agg <- data_agg %>%
+      dplyr::rename(vDim = tidyselect::all_of(vertical_dim))
+  }
+
+
 
   #...........................
   # Plot
@@ -67,6 +82,19 @@ plot_class_waterfall <- function(data_diff = NULL,
   count <- 1
 
   for(i in 1:length(unique(data_diff$param))){
+    # filter to single parameter
+    data_diff_param <- data_diff_subset %>%
+      dplyr::filter(param == unique(data_diff$param)[i])
+    data_agg_param <- data_agg %>%
+      dplyr::filter(param == unique(data_diff$param)[i])
+
+    # give value of zero to class and vDim combinations that don't exist
+    all_classes <- unique(data_diff_param$class)
+    data_diff_param <- data_diff_param %>%
+      tidyr::pivot_wider(names_from = "class", values_from = "value") %>%
+      replace(is.na(.), 0) %>%
+      tidyr::pivot_longer(all_of(all_classes), names_to = "class", values_to = "value")
+
 
     # Check Color Palettes ....................................
     palCustom <- palette
@@ -96,31 +124,57 @@ plot_class_waterfall <- function(data_diff = NULL,
     names(palTotal) <- c(scenRef, scenDiff)
     palCharts <- c(palCharts, palTotal)
 
+
     for(j in 1:length(scenDiff)){
-      # subset data to the current parameter
-      data_diff_plot <- data_diff_subset %>%
-        dplyr::filter(param == unique(data_diff$param)[i])
-      data_agg_plot <- data_agg %>%
-        dplyr::filter(param == unique(data_diff$param)[i])
+      # initiate lists of refTotals, diffRects, diffTotals
+      refTotals = c()
+      #diffRects = list()
+      diffTotals = c()
+      values = matrix(nrow = length(unique(data_diff_param$vDim)),
+                      ncol = length(unique(data_diff_param$class))+1)
 
-      # get the leftmost rectangle (total of reference case)
-      refTotal <- dplyr::filter(data_agg_plot, x == wf_x,
-                                scenario == scenRef)[["value"]]
+      for(v in 1:length(unique(data_diff_param$vDim))){
+        # subset data to the current parameter and vertical dimension
+        data_diff_plot <- data_diff_param %>%
+          dplyr::filter(vDim == unique(data_diff_param$vDim)[v]) %>%
+          dplyr::arrange(class)
+        data_agg_plot <- data_agg_param %>%
+          dplyr::filter(vDim == unique(data_diff_param$vDim)[v]) %>%
+          dplyr::arrange(class)
 
-      # get values for the rest of the rectangle (diffs)
-      diffRects <- data_diff_plot %>%
-        dplyr::filter(scenario == paste0(scenDiff[j], "_", diff_text, "_", scenRef))
+        # get the leftmost rectangle (total of reference case)
+        refTotals[v] <- dplyr::filter(data_agg_plot, x == wf_x,
+                                  scenario == scenRef)[["value"]]
+        diffTotals[v] <- dplyr::filter(data_agg_plot, x == wf_x,
+                                        scenario == scenDiff[j])[["value"]]
+
+        # get values for the rest of the rectangle (diffs)
+        diffRects <- data_diff_plot %>%
+          dplyr::filter(scenario == paste0(scenDiff[j], "_", diff_text, "_", scenRef))
+
+        values[v,] <- c(0, diffRects$value)
+
+      }
 
       # get all classes and values in order (excluding the rightmost rectangle)
-      classes <- factor(c(scenRef, diffRects$class, scenDiff[j]),
-                        levels = c(scenRef, diffRects$class, scenDiff[j]))
-      values <- c(0, diffRects$value)
+      classes <- factor(c(scenRef, sort(unique(data_diff_param$class)), scenDiff[j]),
+                        levels = c(scenRef, sort(unique(data_diff_param$class)), scenDiff[j]))
 
       # calculate the cumulative sum of the values (these correspond to the
       # tops and bottoms of the diff rectangles)
-      currentVal <- c(refTotal + cumsum(values), 0)
-      prevVal <- c(0, refTotal + cumsum(values))
+      currentValTotal <- c(sum(refTotals) + cumsum(colSums(values)), 0)
+      prevValTotal <- c(0, sum(refTotals) + cumsum(colSums(values)))
+      # calculate the heights of the stacked rectangles
+      midVals <- matrix(ncol = length(unique(data_diff_param$class))+2,
+                        nrow = length(unique(data_diff_param$vDim)))
+      midVals[,1] <- cumsum(refTotals)
+      for(c in 2:(length(unique(data_diff_param$class))+1)){
+        midVals[,c] <- prevValTotal[c] + cumsum(values[,c])
+      }
+      midVals[,ncol(midVals)] <- sum(diffTotals) -cumsum(diffTotals)
 
+      currentVal <- midVals
+      prevVal <- rbind(prevValTotal, midVals[1:(nrow(midVals)-1),])
 
       # set fill colors
       if(is.null(fill_colors)){
@@ -137,23 +191,33 @@ plot_class_waterfall <- function(data_diff = NULL,
         ggplot2::geom_hline(yintercept = 0) +
         theme_default
 
+      # amounts to darken each class color for each vertical dim
+      darken <- seq(from = 0, to = 0.4, length.out = length(unique(data_diff_param$vDim)))
+
       for(k in 1:length(classes)){
-        # add rectangles
-        p <- p + ggplot2::annotate("rect",
-                                   xmin = k - rect_width/2,
-                                   xmax = k + rect_width/2,
-                                   ymin = min(currentVal[k], prevVal[k]),
-                                   ymax = max(currentVal[k], prevVal[k]),
-                                   fill = fill_colors[as.character(classes[k])],
-                                   color = "black")
+        for(v in 1:length(unique(data_diff_param$vDim))){
+          # add rectangles
+          p <- p + ggplot2::annotate("rect",
+                                     xmin = k - rect_width/2,
+                                     xmax = k + rect_width/2,
+                                     ymin = min(prevVal[v,k], currentVal[v,k]),
+                                     ymax = max(prevVal[v,k], currentVal[v,k]),
+                                     #ymin = min(currentVal[k], prevVal[k]),
+                                     #ymax = max(currentVal[k], prevVal[k]),
+                                     fill = colorspace::darken(
+                                       fill_colors[as.character(classes[k])],
+                                       amount = darken[v]),
+                                     color = "black")
+        }
+
 
         # add lines connecting rectangles
         if(horizontal_lines && k > 1){
           p <- p + ggplot2::annotate("segment",
                                      x = k - rect_width/2,
                                      xend = k - 1 + rect_width/2,
-                                     y = prevVal[k],
-                                     yend = prevVal[k],
+                                     y = prevValTotal[k],
+                                     yend = prevValTotal[k],
                                      lty = lty)
         }
       }
